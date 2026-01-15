832d27bed3418332f4cf9d7684c226856967310f
fcmello <fabioc.mello@gmail.com>
2026-01-15 16:57:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,20 +29,22 @@ app.post("/render-mp4", upload.single("file"), (req, res) => {
   const duration = Number(req.body.duration || 3);
 
   // 🔒 FFmpeg SIMPLES E SEGURO
-  const cmd =
-    `ffmpeg -y -loop 1 -i ${input} ` +
-    `-vf "scale=1920:1080:force_original_aspect_ratio=increase,crop=1920:1080" ` +
-    `-t ${duration} -r 30 -pix_fmt yuv420p ${output}`;
+ let filter;
 
-  exec(cmd, (err) => {
-    if (err) {
-      console.error("FFmpeg error:", err);
-      return res.status(500).json({ error: "FFmpeg failed" });
-    }
+if (req.body.motion === "zoom_in") {
+  filter = "zoompan=z='1+0.0015*on':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':s=1920x1080:d=1";
+}
+else if (req.body.motion === "zoom_out") {
+  filter = "zoompan=z='1.5-0.0015*on':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':s=1920x1080:d=1";
+}
+else {
+  filter = "zoompan=z=1.2:s=1920x1080:d=1";
+}
 
-    res.download(output, () => {
-      fs.unlinkSync(input);
-      fs.unlinkSync(output);
+const cmd =
+  `ffmpeg -y -loop 1 -i ${input} ` +
+  `-vf "${filter},scale=1920:1080,setsar=1" ` +
+  `-t ${duration} -r 30 -pix_fmt yuv420p ${output}`;
     });
   });
 });
